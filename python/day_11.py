283f8d43113b5fded448f83de85051af070e394a
Scott Minor <minor@minorhacks.com>
2022-12-11 01:47:08 -0500

    Finished day 11

--- python/day_11.py
+++ python/day_11.py
@@ -2,6 +2,7 @@ import collections
 import functools
 import operator
 import itertools
+import math
 from typing import List
 
 from absl import logging as log
@@ -28,9 +29,9 @@ class Monkey:
             lines[5].strip().removeprefix("If false: throw to monkey ")
         )
 
-    def take_turn(self):
+    def take_turn(self, lcm, worry_factor=1):
         while self.items:
-            self.process_item()
+            self.process_item(lcm, worry_factor)
 
     def catch(self, item: int) -> None:
         if item is None:
@@ -40,15 +41,22 @@ class Monkey:
     def inspect_count(self):
         return self._inspect_count
 
-    def process_item(self):
+    def process_item(self, lcm, worry_factor):
         old = self.items.popleft()
         log.debug("  Monkey inspects an item with a worry level of %d.", old)
         new = eval(self.operation, {}, {"old": old})
         log.debug("    Worry level is now %d", new)
-        new = new // 3
-        log.debug(
-            "    Monkey gets bored with item. Worry level is divided by 3 to %d.", new
-        )
+
+        if worry_factor != 1:
+            new = new // worry_factor
+            log.debug(
+                "    Monkey gets bored with item. Worry level is divided by %d to %d.",
+                worry_factor,
+                new,
+            )
+        else:
+            new = new % lcm
+
         self._inspect_count += 1
 
         if new % self.modulus:
@@ -69,20 +77,35 @@ class Monkey:
             self.pool[self.divisible_idx].catch(new)
 
 
-def part_1(input: str) -> int:
+def monkey_pool(input: str) -> List[Monkey]:
     blocks = [i for i in input.strip().split("\n\n")]
     pool = [Monkey() for _ in range(len(blocks))]
     [m.init(pool, blocks[i]) for (i, m) in enumerate(pool)]
-    for _ in range(20):
-        for (i, monkey) in enumerate(pool):
-            log.debug("Monkey %d", i)
-            monkey.take_turn()
-    inspect_counts = [m.inspect_count() for m in pool]
+    return pool
+
+
+def monkey_business(pool: List[Monkey]) -> int:
     return functools.reduce(
         operator.mul,
         itertools.islice(sorted((m.inspect_count() for m in pool), reverse=True), 2),
     )
 
 
+def part_1(input: str) -> int:
+    pool = monkey_pool(input)
+    lcm = math.lcm(*[m.modulus for m in pool])
+    for _ in range(20):
+        for (i, monkey) in enumerate(pool):
+            log.debug("Monkey %d", i)
+            monkey.take_turn(lcm=lcm, worry_factor=3)
+    return monkey_business(pool)
+
+
 def part_2(input: str) -> int:
-    pass
+    pool = monkey_pool(input)
+    lcm = math.lcm(*[m.modulus for m in pool])
+    for turn in range(10000):
+        log.info("Turn %d", turn)
+        for (i, monkey) in enumerate(pool):
+            monkey.take_turn(lcm=lcm)
+    return monkey_business(pool)
